a42346970d5f05df26a7ced2f303ad539b5fd6ac
Amorlot <amorlot99@gmail.com>
2026-06-03 17:22:54 +0200

    push finale - progetto completo

--- auto_mpg/__init__.py
+++ auto_mpg/__init__.py
@@ -3,3 +3,5 @@ from .data_cleaner import DataCleaner
 from .feature_selector import FeatureSelector
 from .preprocessor import Preprocessor
 from .eda import Eda
+from .split import Split
+from .linreg import LinReg
